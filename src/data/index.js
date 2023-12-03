03587ebeea83af6f5ae0cc91dfb28ee5e8dfb806
panjiyudasetya <panjiyudasetya@gmail.com>
2023-12-04 01:21:18 +0700

    Reduce dataset

--- src/data/index.js
+++ src/data/index.js
@@ -46,39 +46,7 @@ const employeesData = [
     email: 'laurena@example.com',
     salary: '120000',
     date: '2019-07-30',
-  },
-  {
-    id: 7,
-    firstName: 'George',
-    lastName: 'Tallman',
-    email: 'george@example.com',
-    salary: '90000',
-    date: '2019-08-15',
-  },
-  {
-    id: 8,
-    firstName: 'Jesica',
-    lastName: 'Watlington',
-    email: 'jesica@example.com',
-    salary: '60000',
-    date: '2019-10-10',
-  },
-  {
-    id: 9,
-    firstName: 'Matthew',
-    lastName: 'Warren',
-    email: 'matthew@example.com',
-    salary: '71000',
-    date: '2019-10-15',
-  },
-  {
-    id: 10,
-    firstName: 'Lyndsey',
-    lastName: 'Follette',
-    email: 'lyndsey@example.com',
-    salary: '110000',
-    date: '2020-01-15',
-  },
+  }
 ];
 
 export { employeesData };
